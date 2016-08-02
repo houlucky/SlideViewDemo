b8c48f79eca847fad5ef56cd0d51aa2ef292e8db
houxy_lucky <houxy_lucky@sina.cn>
2016-08-02 11:50:13 +0800

    头像动画

---
+++ app/src/main/java/com/houxy/slideviewdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.houxy.slideviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
